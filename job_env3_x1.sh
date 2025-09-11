e82188af7fcc52e537c400329d9301a7b9ba39a9
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-09-11 15:13:44 -0600

    new job file

--- job_env3_x1.sh
+++ job_env3_x1.sh
@@ -1,16 +1,21 @@
+
 #!/bin/bash
 
 #SBATCH --job-name=TD3      ## Name of the job
-#SBATCH --output=TD3.out    ## Output file
 #SBATCH --time=24:00:00           ## Job Duration
 #SBATCH --ntasks=1             ## Number of tasks (analyses) to run
 #SBATCH --cpus-per-task=8      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=2G     ## Real memory(MB) per CPU required by the job.
+ARG=${1:-1}
+LOGFILE="TD3_${ARG}.out"
+exec >"$LOGFILE" 2>&1              # send all output (stdout+stderr) to the log
 
 ## not using the default python
 module purge
 
 ## Execute the python script and pass the argument/input '90'
+ARG=$1   # grab the first argument passed to sbatch
 source ~/miniconda3/bin/activate py310
-srun --ntasks=1 --cpus-per-task=8 --mem-per-cpu=2G python TD3_env3_x_main_HPC.py 1
-wait
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+
+srun python TD3_env3_x_main_HPC.py "$ARG"
