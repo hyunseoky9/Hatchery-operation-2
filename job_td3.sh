c2c5051142560e1edfef4c41daec16829d4692ce
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-10-10 16:26:03 -0700

    logging fix

--- job_td3.sh
+++ job_td3.sh
@@ -9,7 +9,7 @@ PARAM_ID=$1
 # Generate a random session ID between 0 and 100000
 SESSION_ID=$(( RANDOM % 100001 ))
 SESSION_NAME="${SESSION_ID}_${PARAM_ID}_TD3"
-LOGFILE="train_${PARAM_ID}_${SESSION_ID}.log"
+LOGFILE="zztrain_${PARAM_ID}_${SESSION_ID}.log"
 
 echo "Launching TD3 training:"
 echo "  paramID: ${PARAM_ID}"
@@ -17,7 +17,7 @@ echo "  tmux session: ${SESSION_NAME}"
 echo "  log file: ${LOGFILE}"
 
 # Start tmux session
-tmux new-session -d -s "$SESSION_NAME" "source ~/miniconda3/bin/activate hatchery && cd ~/Hatchery-operation-2 && python TD3_env3_x_main_HPC.py ${PARAM_ID} | tee -a ${LOGFILE}"
+tmux new-session -d -s "$SESSION_NAME" "source ~/miniconda3/bin/activate hatchery && cd ~/Hatchery-operation-2 && python -u TD3_env3_x_main_HPC.py ${PARAM_ID} 2>&1 | tee -a ${LOGFILE}"
 
 echo "Training started in tmux session '${SESSION_NAME}'."
 echo "To attach:   tmux attach -t ${SESSION_NAME}"
