8be84c0666e5eccc349987c71626df898385d4e8
Hyun Seok Yoon <hyunseoky9@gmail.com>
2026-01-16 00:08:01 -0700

    output file change

--- job_env3_x2.sh
+++ job_env3_x2.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=8      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=16G     ## Real memory(MB) per CPU required by the job.
 ARG=${1:-1}
-LOGFILE="TD3_${ARG}_${SLURM_JOB_ID}.out"
+LOGFILE="zztrain_${ARG}_${SLURM_JOB_ID}.log"
 exec >"$LOGFILE" 2>&1              # send all output (stdout+stderr) to the log
 
 ## not using the default python
